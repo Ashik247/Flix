d6ebc64e765ee7bfe8ea69282b04fda6f0b059fb
Ashik Chowdhury <zero@ashiks-macbook-pro.local>
2019-02-23 17:33:28 -0500

    Trailer video working through dispatch queue

--- Flix/WebViewController.swift
+++ Flix/WebViewController.swift
@@ -26,49 +26,61 @@ class WebViewController: UIViewController, WKUIDelegate  {
     var movies = [[String:Any]] ()
     var movie: [String:Any]!
     var movieKey = ""
+    
+    let dispatchGroup = DispatchGroup()
     @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.uiDelegate = self
-        
-        let movieId = movie["id"]
-        print (movieId!)
-        
-        DispatchQueue.global(qos: .background).sync  {
 
-        
-        let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieId!)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            // This will run when the network request returns
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-//                let movieResults = dataDictionary["results"]
-//                print(movieResults)
-                self.movies = dataDictionary["results"] as! [[String:Any]]
-                let movieFirst = self.movies.first as! [String:Any]
-                self.movieKey = movieFirst["key"] as! String
-                print(self.movieKey)
-                
-                
-                
-            }
-        }
-        task.resume()
-        }
+            getVideo()
 
-        let myUrl = URL(string: "https://www.youtube.com/watch?v=\(movieKey)")!
-        print(movieKey)
-       print(myUrl)
-        webView.load(URLRequest(url: myUrl))
 
         
     }
     
+    func run (after seconds:Int, completion: @escaping() -> Void){
+        let deadline = DispatchTime.now() + .seconds(seconds)
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
+            completion()
+        }
+    }
+    
+    func getVideo(){
+        dispatchGroup.enter()
+        let movieId = movie["id"]
+        print (movieId!)
+        run(after: 5) {
+            let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieId!)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { (data, response, error) in
+                // This will run when the network request returns
+                if let error = error {
+                    print(error.localizedDescription)
+                } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    //                let movieResults = dataDictionary["results"]
+                    //                print(movieResults)
+                    self.movies = dataDictionary["results"] as! [[String:Any]]
+                    let movieFirst = self.movies.first as! [String:Any]
+                    self.movieKey = movieFirst["key"] as! String
+                    print(self.movieKey)
+                    self.dispatchGroup.leave()
+
+                }
+            }
+            task.resume()
+        }
+        dispatchGroup.notify(queue: .main) {
+            let myUrl = URL(string: "https://www.youtube.com/watch?v=\(self.movieKey)")!
+            print(self.movieKey)
+            print(myUrl)
+            self.webView.load(URLRequest(url: myUrl))
+        }
+        
+    }
     
     /*
      // MARK: - Navigation
